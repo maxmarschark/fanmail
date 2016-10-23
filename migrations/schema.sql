378890726c30628d430e3ac108c39ba0e8aaaa2e
max marschark <maxmarschark@gmail.com>
2016-10-23 19:00:09 -0400

    db files

---
+++ migrations/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
---
+++ migrations/seed.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE users;
+
+INSERT INTO users(name, email) VALUES ('Max Marschark', 'max@test.com');
